8c4730a27271c220289da8607d42685bef977e14
E Rippey <rippey.e@gmail.com>
2018-03-25 18:14:01 -0700

    More work on giving percentage odds of DCMP results

--- cutoff.py
+++ cutoff.py
@@ -2,7 +2,21 @@
 
 #Look at historical qualification data to figure out cutoffs.
 
+import numpy as np
 from argparse import ArgumentParser
+from glob import glob
+from predictor import flatten
+from meta import print_lines
+
+def firsts(a):
+	#any(a) => [[a]]->[a]
+	return map(lambda x: x[0],a)
+
+
+def seconds(a):
+	#any(a) => [[a]]->[a]
+	return map(lambda x: x[1],a)
+
 
 def parse_line(s):
 	sp=s.split('\t')
@@ -41,12 +55,88 @@ def none_to_0(x):
 		return 0
 	return x
 
+#TODO: Make it so that do the least squares for interesting percentiles
+#so like 25th, 50th, 75th, 10th, 90th?
+#or do this at every number of points?
+#so odds that will make at least X points from each starting number of points
+def calc_lines():
+	f=flatten(map(parse_file,glob('data/20*_pnw_post.txt')))
+	m=map(lambda x: (x['event1'],x['event2']),f)
+	m=filter(lambda x: x[0] is not None and x[1] is not None,m)
+	#print m[:10]
+	#for e1_score in sorted(set(firsts(m))):
+	#	print e1_score
+	out={}
+	for target_score in range(80):
+		#print target_score,
+		m1=map(lambda x: (x[0],x[1]>target_score),m)
+		#print m1
+		par=np.polyfit(firsts(m1), seconds(m1), 1, full=True)
+		slope=par[0][0]
+		intercept=par[0][1]
+		def f(x): return slope*x+intercept
+		#print '\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f'%(slope,intercept,f(4),f(20),f(30),f(50))
+		out[target_score]=(slope,intercept)
+	return out
+
+#now just need to combine with a probability of the cutoff being a certain number of points.
+#58 mean
+#stddev of 2?
+#58 .5
+#57/59 .125 each
+#56/60 1/16
+#55/61 1/16
+
+from scipy.stats import norm
+
+def cutoff_odds():
+	#print 'norm:',np.random.normal(58,5)
+
+	out={}
+	for i in range(40,80):
+		out[i]=norm.pdf(i,58,5)
+	#print_lines(sorted(out.iteritems()))
+	return out
+
+	raise 'nyi'
+	return {
+		61:1.0/16,
+		60:1.0/16,
+		59:1.0/8,
+		58:.5,
+		57:1.0/8,
+		56:1.0/16,
+		55:1.0/16
+		}
+
+def lines_demo():
+	lines=calc_lines()
+	
+	for existing_points in range(78):
+		print existing_points,
+		total_px=0
+		for cutoff_points,cutoff_px in cutoff_odds().iteritems():
+			points_needed=cutoff_points-existing_points
+			if points_needed<=0:
+				px=1
+			else:
+				slope,intercept=lines[points_needed]
+				px=slope*existing_points+intercept
+				px=min(px,1)
+				px=max(px,0)
+			total_px+=px*cutoff_px
+		print total_px
+
 if __name__=='__main__':
 	p=ArgumentParser()
 	p.add_argument('--file',default='data/2017_pnw_post.txt')
+	p.add_argument('--mean',action='store_true')
 	args=p.parse_args()
 	data=parse_file(args.file)
-	
-	for a in data:
-		qual_pts=none_to_0(a['event1'])+none_to_0(a['event2'])+a['team_age_points']
-		print qual_pts,a['district_championship'],a['team']
+
+	if args.mean:
+		for a in data:
+			qual_pts=none_to_0(a['event1'])+none_to_0(a['event2'])+a['team_age_points']
+			print qual_pts,a['district_championship'],a['team']
+
+	lines_demo()
