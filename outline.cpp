84c36709134edb9d836fb78e5b63cad461333181
E Rippey <rippey.e@gmail.com>
2019-03-10 23:16:57 -0700

    Chairmans winners & rookie bonus

--- outline.cpp
+++ outline.cpp
@@ -19,8 +19,7 @@ later version could assume that teams have some sort of skill quantified by eith
 #include "tba/tba.h"
 #include "tba/util.h"
 
-using namespace std;
-using namespace tba;
+//start generic stuff
 
 #define PRINT TBA_PRINT
 #define nyi TBA_NYI
@@ -38,23 +37,112 @@ std::string slurp(std::string const& filename){
 template<typename T>
 void print_lines(T t){
 	for(auto elem:t){
-		cout<<elem<<"\n";
+		std::cout<<elem<<"\n";
 	}
 }
 
 template<typename T>
-multiset<T>& operator|=(multiset<T>& a,T t){
+std::multiset<T>& operator|=(std::multiset<T>& a,T t){
 	a.insert(t);
 	return a;
 }
 
 template<typename T>
-multiset<T>& operator|=(multiset<T>& a,multiset<T> b){
+std::multiset<T>& operator|=(std::multiset<T>& a,std::multiset<T> b){
 	for(auto elem:b){
 		a|=elem;
 	}
 	return a;
 }
+
+template<typename K,typename V>
+std::vector<V> seconds(std::map<K,V> a){
+	std::vector<V> r;
+	for(auto elem:a){
+		r|=elem.second;
+	}
+	return r;
+}
+
+double sum(std::vector<double> v){
+	double r=0;
+	for(auto elem:v){
+		r+=elem;
+	}
+	return r;
+}
+
+template<typename Func,typename T>
+auto mapf(Func f,std::vector<T> const& v)->std::vector<decltype(f(v[0]))>{
+	std::vector<decltype(f(v[0]))> r;
+	for(auto elem:v){
+		r|=f(elem);
+	}
+	return r;
+}
+
+template<typename Func,typename K,typename V>
+auto mapf(Func f,std::map<K,V> const& v)->std::vector<decltype(f(*std::begin(v)))>{
+	std::vector<decltype(f(*std::begin(v)))> r;
+	for(auto p:v){
+		r|=f(p);
+	}
+	return r;
+}
+
+template<typename K,typename V>
+std::map<K,V> to_map(std::vector<std::pair<K,V>> v){
+	std::map<K,V> r;
+	for(auto p:v){
+		auto f=r.find(p.first);
+		assert(f==r.end());
+		r[p.first]=p.second;
+	}
+	return r;
+}
+
+template<typename K,typename V>
+auto values(std::map<K,V> a)->std::vector<V>{
+	std::vector<V> r;
+	for(auto p:a) r|=p.second;
+	return r;
+}
+
+template<typename T>
+std::map<T,size_t> count(std::multiset<T> const& a){
+	std::map<T,size_t> r;
+	for(auto elem:a){
+		r[elem]=a.count(elem); //slow
+	}
+	return r;
+}
+
+template<typename Func,typename K,typename V>
+auto map_values(Func f,std::map<K,V> m)->std::map<K,decltype(f(begin(m)->second))>{
+	std::map<K,decltype(f(begin(m)->second))> r;
+	for(auto [k,v]:m){
+		r[k]=f(v);
+	}
+	return r;
+}
+
+template<typename T,typename Func>
+std::vector<T> sorted(std::vector<T> v,Func f){
+	sort(begin(v),end(v),[&](auto a,auto b){ return f(a)<f(b); });
+	return v;
+}
+
+template<typename T>
+std::vector<T> reversed(std::vector<T> a){
+	reverse(begin(a),end(a));
+	return a;
+}
+
+//start program-specific stuff.
+
+using namespace std;
+using namespace tba;
+
 using Pr=double; //probability
 using Point=int;
 
@@ -66,48 +154,6 @@ multiset<Point> point_results(Cached_fetcher& server,District_key dk){
 		for(auto event:team_result.event_points){
 			r|=Point(event.total);
 		}
-	}
-	return r;
-}
-
-template<typename K,typename V>
-vector<V> seconds(map<K,V> a){
-	vector<V> r;
-	for(auto elem:a){
-		r|=elem.second;
-	}
-	return r;
-}
-
-double sum(vector<double> v){
-	double r=0;
-	for(auto elem:v){
-		r+=elem;
-	}
-	return r;
-}
-
-template<typename Func,typename T>
-auto mapf(Func f,vector<T> const& v)->vector<decltype(f(v[0]))>{
-	nyi
-}
-
-template<typename Func,typename K,typename V>
-auto mapf(Func f,map<K,V> const& v)->vector<decltype(f(*std::begin(v)))>{
-	vector<decltype(f(*std::begin(v)))> r;
-	for(auto p:v){
-		r|=f(p);
-	}
-	return r;
-}
-
-template<typename K,typename V>
-map<K,V> to_map(vector<pair<K,V>> v){
-	map<K,V> r;
-	for(auto p:v){
-		auto f=r.find(p.first);
-		assert(f==r.end());
-		r[p.first]=p.second;
 	}
 	return r;
 }
@@ -129,41 +175,45 @@ map<Point,Pr> convolve(map<Point,Pr> a,map<Point,Pr> b){
 	return r;
 }
 
-template<typename K,typename V>
-auto values(map<K,V> a)->vector<V>{
-	vector<V> r;
-	for(auto p:a) r|=p.second;
-	return r;
-}
-
-template<typename T>
-map<T,size_t> count(multiset<T> const& a){
-	map<T,size_t> r;
+template<typename Func,typename T>
+T filter_unique(Func f,vector<T> a){
+	vector<T> found;
 	for(auto elem:a){
-		r[elem]=a.count(elem); //slow
+		if(f(elem)){
+			found|=elem;
+		}
 	}
-	return r;
+	assert(found.size()==1);
+	return found[0];
 }
 
-template<typename Func,typename K,typename V>
-auto map_values(Func f,map<K,V> m)->map<K,decltype(f(begin(m)->second))>{
-	map<K,decltype(f(begin(m)->second))> r;
-	for(auto [k,v]:m){
-		r[k]=f(v);
+map<Point,Pr> operator+(map<Point,Pr> a,int i){
+	map<Point,Pr> r;
+	for(auto [k,v]:a){
+		r[k+i]=v;
 	}
 	return r;
-}
-
-template<typename T,typename Func>
-vector<T> sorted(vector<T> v,Func f){
-	sort(begin(v),end(v),[&](auto a,auto b){ return f(a)<f(b); });
-	return v;
 }
 
 template<typename T>
-vector<T> reversed(vector<T> a){
-	reverse(begin(a),end(a));
+set<T>& operator|=(set<T>& a,T t){
+	a.insert(t);
 	return a;
+}
+
+set<Team_key> chairmans_winners(Cached_fetcher& f,District_key district){
+	set<Team_key> r;
+	for(auto event:district_events(f,district)){
+		auto k=event.key;
+		auto aw=event_awards(f,k);
+		if(aw.empty()) continue;
+		auto f=filter_unique([](auto a){ return a.award_type==Award_type::CHAIRMANS; },aw);
+		assert(f.recipient_list.size()==1);
+		auto team=f.recipient_list[0].team_key;
+		assert(team);
+		r|=*team;
+	}
+	return r;
 }
 
 int main(int argc,char **argv){
@@ -174,6 +224,9 @@ int main(int argc,char **argv){
 	auto tba_key=slurp("../tba/auth_key");
 	Cached_fetcher f{Fetcher{Nonempty_string{tba_key}},Cache{}};
 	District_key district{"2019pnw"};
+
+	auto team_info=district_teams(f,district);
+
 	//print_lines(district_rankings(f,district));
 	auto d=district_rankings(f,district);
 	assert(d);
@@ -203,28 +256,36 @@ int main(int argc,char **argv){
 	//PRINT(sum(seconds(pr)));
 	//PRINT(old_results.size())
 
+	auto chairmans=chairmans_winners(f,district);
+
 	map<Team_key,map<Point,Pr>> by_team;
 	for(auto team:d1){
+		if(chairmans.count(team.team_key)){
+			continue;
+		}
 		auto events_left=2-team.event_points.size();
 		assert(events_left>=0);
 		by_team[team.team_key]=[&]()->map<Point,Pr>{
 			if(events_left==0){
-				return map<Point,Pr>{{team.point_total,1}};
+				return map<Point,Pr>{{
+					team.event_points[0].total+team.event_points[1].total,
+					1
+				}};
 			}
 			if(events_left==1){
 				return to_map(mapf(
 					[&](auto p){
-						return make_pair(int(p.first+team.point_total),p.second);
+						return make_pair(int(p.first+team.event_points[0].total),p.second);
 					},
 					pr
 				));
 			}
 			if(events_left==2){
-				return convolve(pr,pr);
+				return convolve(pr,pr)+team.rookie_bonus;
 			}
 			PRINT(team);
 			nyi
-		}();
+		}()+team.rookie_bonus;
 	}
 
 	//print_lines(by_team);
@@ -284,36 +345,19 @@ int main(int argc,char **argv){
 		}
 	}
 
-	auto teams_advancing=64;
+	auto teams_advancing=64-chairmans.size();
 	auto teams_competing=sum(values(by_points));
 	auto teams_left_out=teams_competing-teams_advancing;
 
-	/*
-	probability over threshold for each of the teams?
-	and do this for each number
-	then have an expected number at each location?
-	TODO: Probability that cutoff is at least X = 
-	at each cutoff find probability that it's been met
-	then find probability that dist X > dist Y for the team/environment
-	*/
-	/*map<Point,Pr> cutoff_odds=to_map(mapf(
-		[](auto pts){
-			
-		},
-		range(0,100)
-	));*/
-	//convolutions of all of the teams together -> pts->odds
-	//monte carlo method for where the cutoff is?
+	//monte carlo method for where the cutoff is
 
-
-
-    std::mt19937_64 rng;
-    // initialize the random number generator with time-dependent seed
-    uint64_t timeSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-    std::seed_seq ss{uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed>>32)};
-    rng.seed(ss);
-    // initialize a uniform distribution between 0 and 1
-    std::uniform_real_distribution<double> unif(0, 1);
+	std::mt19937_64 rng;
+	// initialize the random number generator with time-dependent seed
+	uint64_t timeSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+	std::seed_seq ss{uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed>>32)};
+	rng.seed(ss);
+	// initialize a uniform distribution between 0 and 1
+	std::uniform_real_distribution<double> unif(0, 1);
 
 	auto sample=[&](map<Point,Pr> const& m)->Point{
 		auto num=unif(rng);
@@ -397,32 +441,45 @@ int main(int argc,char **argv){
 		}
 		//PRINT(pr_make+pr_miss);
 		auto total=pr_make+pr_miss;
-		assert(total>.99 && total<1.01);
+		if(chairmans.count(team.team_key)){
+			assert(total>=-.01 && total<=.01);
+			result|=make_tuple(team.team_key,1.0,0,0,0);
+		}else{
+			assert(total>.99 && total<1.01);
 
-		//PRINT(pr_make);
-		//points needed to have 50% odds; 5% odds; 95% odds, or quartiles?
-		auto points_so_far=team.point_total;
-		vector<Point> interesting;
-		for(auto [pr,pts]:interesting_cutoffs){
-			//cout<<pr<<":"<<max(0.0,pts-points_so_far)<<"\n";
-			auto value=max(0,int(pts-points_so_far));
-			interesting|=value;
+			//PRINT(pr_make);
+			//points needed to have 50% odds; 5% odds; 95% odds, or quartiles?
+			auto points_so_far=team.point_total;
+			vector<Point> interesting;
+			for(auto [pr,pts]:interesting_cutoffs){
+				//cout<<pr<<":"<<max(0.0,pts-points_so_far)<<"\n";
+				auto value=max(0,int(pts-points_so_far));
+				interesting|=value;
+			}
+			assert(interesting.size()==3);
+			result|=make_tuple(team.team_key,pr_make,interesting[0],interesting[1],interesting[2]);
 		}
-		assert(interesting.size()==3);
-		result|=make_tuple(team.team_key,pr_make,interesting[0],interesting[1],interesting[2]);
 	}
 
-	cout<<"Team\tP(DCMP)\tPts 5%\tPts 50%\tPts 95%\n";
-	cout.precision(3);
-	for(auto a:reversed(sorted(
-		result,
-		[](auto x){ return make_pair(get<1>(x),x); }
-	))){
-		cout<<get<0>(a).str().substr(3,100)<<"\t";
-		cout<<get<1>(a)<<"\t";
-		cout<<get<2>(a)<<"\t";
-		cout<<get<3>(a)<<"\t";
-		cout<<get<4>(a)<<"\n";
+	auto x=mapf([](auto x){ return get<1>(x); },result);
+	PRINT(sum(x));
+
+	bool show_table=1;
+	if(show_table){
+		cout<<"Team #\tP(DCMP)\tPts 5%\tPts 50%\tPts 95%\tNickname\n";
+		cout.precision(3);
+		for(auto a:reversed(sorted(
+			result,
+			[](auto x){ return make_pair(get<1>(x),x); }
+		))){
+			cout<<get<0>(a).str().substr(3,100)<<"\t";
+			cout<<get<1>(a)<<"\t";
+			cout<<get<2>(a)<<"\t";
+			cout<<get<3>(a)<<"\t";
+			cout<<get<4>(a)<<"\t";
+			auto f=filter_unique([=](auto f){ return f.key==get<0>(a); },team_info);
+			cout<<f.nickname<<"\n";
+		}
 	}
 
 	return 0;
